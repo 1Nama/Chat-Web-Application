bd514467162243fd30a58ef8f073faa496ad4bcd
Naman Dhanawat <namand0683@gmail.com>
2025-09-22 14:23:47 +0530

    Minor Changes

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
        <h1 className='text-3xl font-semibold text-center text-gray-300'>
        Login
-       <span className='text-blue-400'> ChitChat</span>
+       <span className='text-blue-400'> ChitChatter</span>
        </h1>
        <form onSubmit={handleSubmit}>
         <div>
